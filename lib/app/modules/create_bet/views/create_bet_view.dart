3309cf50afe46d44d63073f68a28cc77afb4e38b
AshishDT <ashish@dharmatec.in>
2025-08-19 15:37:53 +0530

    + Create bet view refactor

--- lib/app/modules/create_bet/views/create_bet_view.dart
+++ lib/app/modules/create_bet/views/create_bet_view.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fvf_flutter/app/data/config/app_colors.dart';
 import 'package:fvf_flutter/app/data/config/app_images.dart';
-import 'package:fvf_flutter/app/modules/create_bet/widgets/keyboard_aware_sheet.dart';
 import 'package:fvf_flutter/app/modules/create_bet/widgets/work_space_sheet_repo.dart';
+import 'package:fvf_flutter/app/routes/app_pages.dart';
 import 'package:fvf_flutter/app/ui/components/animated_list_view.dart';
 import 'package:fvf_flutter/app/ui/components/app_button.dart';
 import 'package:fvf_flutter/app/utils/app_text_style.dart';
@@ -20,95 +20,89 @@ class CreateBetView extends GetView<CreateBetController> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        backgroundColor: AppColors.kF5FCFF,
-        body: Stack(
-          children: [
-            GradientCard(
-              child: Align(
-                alignment: Alignment.topLeft,
-                child: SafeArea(
-                  child: AnimatedListView(
-                    padding: REdgeInsets.symmetric(horizontal: 24),
-                    children: <Widget>[
-                      const CommonAppBar(),
-                      64.verticalSpace,
-                      GradientCard(
-                        padding:
-                            REdgeInsets.symmetric(vertical: 31, horizontal: 24),
-                        borderRadius: BorderRadius.circular(32.r),
-                        constraints: BoxConstraints(maxHeight: 120.h),
-                        bgImage: AppImages.contentCardBg,
-                        child: Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: Text(
-                                'Most likely to start an OF?',
-                                style: AppTextStyle.openRunde(
-                                  color: AppColors.kffffff,
-                                  fontSize: 24.sp,
-                                  fontWeight: FontWeight.w600,
-                                  height: 1.3,
-                                ),
-                              ),
-                            ),
-                            16.horizontalSpace,
-                            Image.asset(AppImages.dice),
-                          ],
-                        ),
-                      ),
-                      24.verticalSpace,
-                      AppButton(
-                        buttonText: '',
-                        buttonColor: AppColors.kE4F7FB,
-                        showGradient: false,
-                        onPressed: () {
-                          WorkSpaceSheetRepo.openChatField();
-                        },
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              'Or write your own',
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                              style: AppTextStyle.openRunde(
-                                fontSize: 18.sp,
-                                fontWeight: FontWeight.w600,
-                                color: AppColors.k3D4445,
-                              ),
-                            ),
-                            4.horizontalSpace,
-                            Image.asset(
-                              AppImages.pencilIcon,
-                              height: 32.h,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 35.h,
-              left: 24.w,
-              right: 24.w,
-              child: AppButton(
-                buttonText: 'Bet',
-                onPressed: () {},
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    colors: <Color>[
-                      AppColors.kA68DF5,
-                      AppColors.k64B6FE,
-                    ],
-                  ),
-                  borderRadius: BorderRadius.circular(28).r,
-                ),
-              ),
-            ),
+    backgroundColor: AppColors.kF5FCFF,
+    floatingActionButton: AppButton(
+      buttonText: 'Bet',
+      onPressed: () {
+        Get.toNamed(Routes.PICK_CREW);
+      },
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          colors: <Color>[
+            AppColors.kA68DF5,
+            AppColors.k64B6FE,
           ],
         ),
-      );
+        borderRadius: BorderRadius.circular(28).r,
+      ),
+    ).paddingSymmetric(horizontal: 24),
+    floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    body: GradientCard(
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: SafeArea(
+          child: AnimatedListView(
+            padding: REdgeInsets.symmetric(horizontal: 24),
+            children: <Widget>[
+              const CommonAppBar(),
+              64.verticalSpace,
+              GradientCard(
+                padding:
+                REdgeInsets.symmetric(vertical: 31, horizontal: 24),
+                borderRadius: BorderRadius.circular(32.r),
+                constraints: BoxConstraints(maxHeight: 120.h),
+                bgImage: AppImages.contentCardBg,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        'Most likely to start an OF?',
+                        style: AppTextStyle.openRunde(
+                          color: AppColors.kffffff,
+                          fontSize: 24.sp,
+                          fontWeight: FontWeight.w600,
+                          height: 1.3,
+                        ),
+                      ),
+                    ),
+                    16.horizontalSpace,
+                    Image.asset(AppImages.dice),
+                  ],
+                ),
+              ),
+              24.verticalSpace,
+              AppButton(
+                buttonText: '',
+                buttonColor: AppColors.kE4F7FB,
+                showGradient: false,
+                onPressed: () {
+                  WorkSpaceSheetRepo.openChatField();
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Or write your own',
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: AppTextStyle.openRunde(
+                        fontSize: 18.sp,
+                        fontWeight: FontWeight.w600,
+                        color: AppColors.k3D4445,
+                      ),
+                    ),
+                    4.horizontalSpace,
+                    Image.asset(
+                      AppImages.pencilIcon,
+                      height: 32.h,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
 }
--- lib/app/modules/create_bet/widgets/keyboard_aware_sheet.dart
+++ lib/app/modules/create_bet/widgets/keyboard_aware_sheet.dart
@@ -15,86 +15,81 @@ class KeyboardAwareSheet extends GetView<CreateBetController> {
   const KeyboardAwareSheet({super.key});
 
   @override
-  Widget build(BuildContext context) => Padding(
-        padding: REdgeInsets.only(
-          bottom: MediaQuery.of(context).viewInsets.bottom,
-        ),
-        child: GradientCard(
-          alignment: AlignmentDirectional.topStart,
-          borderRadius: BorderRadius.vertical(
-            top: Radius.circular(24.r),
-          ),
-          constraints: BoxConstraints(
-            maxHeight: 174.h,
-          ),
-          padding: REdgeInsets.only(
-            top: 24,
-            left: 24,
-            right: 24,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'Write Your Own',
-                style: AppTextStyle.openRunde(
-                  color: AppColors.kffffff,
-                  fontSize: 24.sp,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              16.verticalSpace,
-              AnimatedContainer(
-                duration: const Duration(milliseconds: 300),
-                padding: REdgeInsets.symmetric(horizontal: 16),
-                decoration: BoxDecoration(
-                  color: AppColors.kF1F2F2,
-                  borderRadius: BorderRadius.circular(28).r,
-                ),
-                child: TextFormField(
-                  controller: controller.chatInputController,
-                  focusNode: controller.chatInputFocusNode,
-                  maxLines: 7,
-                  minLines: 1,
-                  autofocus: true,
-                  cursorColor: AppColors.k2A2E2F,
-                  onFieldSubmitted: (String value) {
-                    controller.chatInputFocusNode.unfocus();
-                  },
-                  style: AppTextStyle.openRunde(
-                    color: AppColors.k787C82,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16.sp,
-                  ),
-                  textInputAction: TextInputAction.go,
-                  decoration: InputDecoration(
-                    hintText: 'Message',
-                    prefixIconConstraints: BoxConstraints(
-                      maxHeight: 24.h,
-                      maxWidth: 24.w,
-                    ),
-                    prefixIcon: SvgPicture.asset(
-                      AppImages.penIcon,
-                      height: 24.h,
-                      width: 24.w,
-                    ),
-                    hintStyle: AppTextStyle.openRunde(
-                      color: AppColors.k787C82,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16.sp,
-                    ),
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    disabledBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    errorBorder: InputBorder.none,
-                    focusedErrorBorder: InputBorder.none,
-                  ),
-                ),
-              ),
-            ],
+  Widget build(BuildContext context) => GradientCard(
+    alignment: AlignmentDirectional.topStart,
+    borderRadius: BorderRadius.vertical(
+      top: Radius.circular(24.r),
+    ),
+    constraints: BoxConstraints(
+      maxHeight: 174.h,
+    ),
+    padding: REdgeInsets.only(
+      top: 24,
+      left: 24,
+      right: 24,
+    ),
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'Write Your Own',
+          style: AppTextStyle.openRunde(
+            color: AppColors.kffffff,
+            fontSize: 24.sp,
+            fontWeight: FontWeight.w600,
           ),
         ),
-      );
+        16.verticalSpace,
+        AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          padding: REdgeInsets.symmetric(horizontal: 16, vertical: 6),
+          decoration: BoxDecoration(
+            color: AppColors.kF1F2F2,
+            borderRadius: BorderRadius.circular(28).r,
+          ),
+          child: TextFormField(
+            controller: controller.chatInputController,
+            focusNode: controller.chatInputFocusNode,
+            maxLines: 7,
+            minLines: 1,
+            autofocus: true,
+            cursorColor: AppColors.k2A2E2F,
+            onFieldSubmitted: (String value) {
+              controller.chatInputFocusNode.unfocus();
+            },
+            style: AppTextStyle.openRunde(
+              color: AppColors.k787C82,
+              fontWeight: FontWeight.w600,
+              fontSize: 16.sp,
+            ),
+            textInputAction: TextInputAction.go,
+            decoration: InputDecoration(
+              hintText: 'Message',
+              prefixIconConstraints: BoxConstraints(
+                maxHeight: 24.h,
+                maxWidth: 24.w,
+              ),
+              prefixIcon: SvgPicture.asset(
+                AppImages.penIcon,
+                height: 24.h,
+                width: 24.w,
+              ),
+              hintStyle: AppTextStyle.openRunde(
+                color: AppColors.k787C82,
+                fontWeight: FontWeight.w600,
+                fontSize: 16.sp,
+              ),
+              border: InputBorder.none,
+              focusedBorder: InputBorder.none,
+              disabledBorder: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              errorBorder: InputBorder.none,
+              focusedErrorBorder: InputBorder.none,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
 }

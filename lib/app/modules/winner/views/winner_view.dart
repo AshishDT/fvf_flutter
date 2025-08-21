18671788b738b3efb392ccb78019544ed12a31be
Dhruv <dhruv@dharmatech.in>
2025-08-21 12:22:18 +0530

    + WIP winner view

--- lib/app/modules/winner/views/winner_view.dart
+++ lib/app/modules/winner/views/winner_view.dart
@@ -1,8 +1,11 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fvf_flutter/app/modules/winner/widgets/expose_sheet.dart';
 import 'package:fvf_flutter/app/ui/components/app_button.dart';
 import 'package:get/get.dart';
+
 import '../../../data/config/app_colors.dart';
 import '../../../data/config/app_images.dart';
 import '../../../ui/components/animated_list_view.dart';
@@ -22,10 +25,46 @@ class WinnerView extends GetView<WinnerController> {
   Widget build(BuildContext context) => Scaffold(
         backgroundColor: AppColors.kF5FCFF,
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: AppButton(
-          buttonText: 'Share',
-          onPressed: () {},
-        ).paddingSymmetric(horizontal: 24),
+        floatingActionButton: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            AppButton(
+              buttonText: '',
+              buttonColor: AppColors.kFFC300,
+              onPressed: () {
+                ExposeSheet.openExposeSheet();
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Expose Everyone 👀',
+                    style: AppTextStyle.openRunde(
+                      fontSize: 18.sp,
+                      color: AppColors.k2A2E2F,
+                      fontWeight: FontWeight.w700,
+                      height: .8,
+                    ),
+                  ),
+                  4.verticalSpace,
+                  Text(
+                    'See how everyone placed!',
+                    style: AppTextStyle.openRunde(
+                      fontSize: 12.sp,
+                      color: AppColors.k2A2E2F,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            16.verticalSpace,
+            AppButton(
+              buttonText: 'Share',
+              onPressed: () {},
+            ),
+          ],
+        ).paddingSymmetric(horizontal: 24.w),
         body: GradientCard(
           child: SafeArea(
             child: AnimatedListView(
@@ -100,9 +139,56 @@ class WinnerView extends GetView<WinnerController> {
                     ),
                   ),
                 ),
+                24.verticalSpace,
+                Row(
+                  children: <Widget>[
+                    _emojiTile(
+                      emojiIcon: AppImages.fireIcon,
+                      value: '2',
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
         ),
+      );
+
+  Widget _emojiTile({
+    required String value,
+    required String emojiIcon,
+  }) =>
+      Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            height: 36.w,
+            width: 36.w,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  offset: Offset(0, 2.h),
+                  blurRadius: 2.r,
+                  color: AppColors.k000000.withValues(alpha: 0.2),
+                ),
+              ],
+            ),
+            child: SvgPicture.asset(
+              emojiIcon,
+              height: 36.w,
+              width: 36.w,
+            ),
+          ),
+          2.verticalSpace,
+          Text(
+            value,
+            style: AppTextStyle.openRunde(
+              fontSize: 12.sp,
+              color: AppColors.kffffff,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
       );
 }

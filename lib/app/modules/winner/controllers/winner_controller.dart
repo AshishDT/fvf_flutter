200c7f1982d90731fe0c96caf333170c8135c218
Dhruv <dhruv@dharmatech.in>
2025-08-26 13:50:09 +0530

    + Update winner view

--- lib/app/modules/winner/controllers/winner_controller.dart
+++ lib/app/modules/winner/controllers/winner_controller.dart
@@ -17,10 +17,7 @@ class WinnerController extends GetxController {
   /// On init
   @override
   void onInit() {
-    pageController = PageController(initialPage: 0);
-    participants.sort((MdParticipant a, MdParticipant b) =>
-        a.rank?.compareTo(b.rank ?? 0) ?? 0);
-    /*if (Get.arguments != null) {
+    if (Get.arguments != null) {
       if (Get.arguments['participants'] != null) {
         final List<MdParticipant> _selfies = Get.arguments['participants'] as List<MdParticipant>;
 
@@ -41,7 +38,7 @@ class WinnerController extends GetxController {
         bet.value = Get.arguments['bet'] as String;
         bet.refresh();
       }
-    }*/
+    }
     super.onInit();
   }
 
@@ -82,43 +79,5 @@ class WinnerController extends GetxController {
   }
 
   /// List of selfies taken by the user
-  RxList<MdParticipant> participants = <MdParticipant>[
-    MdParticipant(
-      selfieUrl:
-          'https://images.unsplash.com/photo-1551847812-f815b31ae67c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8c2VsZmllfGVufDB8fDB8fHww',
-      rank: 2,
-      userData: RoundHost(
-        username: 'Alice Johnson',
-        age: 28,
-      ),
-    ),
-    MdParticipant(
-      selfieUrl:
-          'https://images.unsplash.com/photo-1688597168861-b2d5f521cca6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHNlbGZpZXxlbnwwfHwwfHx8MA%3D%3D',
-      isHost: true,
-      rank: 1,
-      userData: RoundHost(
-        username: 'Bob Smith',
-        age: 20,
-      ),
-    ),
-    MdParticipant(
-      selfieUrl:
-          'https://images.unsplash.com/photo-1522556189639-b150ed9c4330?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2VsZmllfGVufDB8fDB8fHww',
-      rank: 3,
-      userData: RoundHost(
-        username: 'Diana Prince',
-        age: 20,
-      ),
-    ),
-    MdParticipant(
-      selfieUrl:
-          'https://images.unsplash.com/photo-1612000529646-f424a2aa1bff?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHNlbGZpZXxlbnwwfHwwfHx8MA%3D%3D',
-      rank: 4,
-      userData: RoundHost(
-        username: 'Catherine Lee',
-        age: 32,
-      ),
-    ),
-  ].obs;
+  RxList<MdParticipant> participants = <MdParticipant>[].obs;
 }
